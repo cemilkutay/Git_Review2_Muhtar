6b4dcaeb877bb7c78790cdd1ba2195cd5a75b19f
cemil kutay <kutaytinmaz@gmail.com>
2023-01-15 20:28:05 +0100

    Initial Commit

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -1,4 +1,10 @@
 package first_package;
 
 public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Class");
+
+    }
 }
--- src/first_package/SecondClass.java
+++ src/first_package/SecondClass.java
@@ -1,4 +1,9 @@
 package first_package;
 
 public class SecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Second Class");
+    }
 }
