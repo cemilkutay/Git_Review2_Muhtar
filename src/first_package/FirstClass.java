096cda4439774b69b112038f0ed7884b79c755cb
Cemil Kutay Tinmaz <114428070+cemilkutay@users.noreply.github.com>
2023-01-15 20:30:01 +0100

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        //some changes are made
+        System.out.println("First Class");
 
     }
 }
